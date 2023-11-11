248598a6b4cbe10acd285a89243c5abbbd4264e5
JackieOlexa <jackie.olexa@umconnect.umt.edu>
2023-11-10 23:28:10 -0700

    user model and blank template

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "country",
+        "status",
+        "is_staff",
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("country",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("country",)}),)
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    country = models.CharField(max_length = 30, null=False, blank=False)
+
+    class StatusRank(models.TextChoices):
+        Egg = "Egg"
+        Hatchling = "Hatchling"
+        Nestling = "Nestling"
+        Fledgeling = "Fledgeling"
+        Flapping = "Flapping"
+        Flying = "Flying"
+        Soaring = "Soaring"
+        
+    status = models.CharField(
+        max_length = 10,
+        choices = StatusRank.choices,
+        default = StatusRank.Egg,
+        null = False,
+    )
+
+    numContributions = models.IntegerField(default = 0, null=False)
+
+    
