3498a3b1c293663746f2725a20078d987ad867c4
Abby Kabalin <ackabalin@yahoo.com>
2023-11-11 18:59:25 -0700

    templates and registration

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,21 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
+
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("country",)
+        fields = UserCreationForm.Meta.fields + (
+            "email",
+            "country",
+        )
+
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = (  # I had to get rid of UserChangeForm.Meta.fields because it was causing an error
+            # and crashing my server
+            "email",
+            "country",
+        )
--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
 class CustomUser(AbstractUser):
-    country = models.CharField(max_length = 30, null=False, blank=False)
+    email = models.CharField(max_length=50, null=False, blank=False)
+
+    country = models.CharField(max_length=30, null=False, blank=False)
 
     class StatusRank(models.TextChoices):
         Egg = "Egg"
@@ -13,14 +16,12 @@ class CustomUser(AbstractUser):
         Flapping = "Flapping"
         Flying = "Flying"
         Soaring = "Soaring"
-        
+
     status = models.CharField(
-        max_length = 10,
-        choices = StatusRank.choices,
-        default = StatusRank.Egg,
-        null = False,
+        max_length=10,
+        choices=StatusRank.choices,
+        default=StatusRank.Egg,
+        null=False,
     )
 
-    numContributions = models.IntegerField(default = 0, null=False)
-
-    
+    numContributions = models.IntegerField(default=0, null=False)
