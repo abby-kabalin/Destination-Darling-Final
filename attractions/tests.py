ebfd8bfb42aadd42471ab32ce73bf5701a97fd2e
Abby Kabalin <ackabalin@yahoo.com>
2023-12-12 23:11:50 -0700

    testing fixes

--- attractions/tests.py
+++ attractions/tests.py
@@ -1,3 +1,115 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+from .models import Attraction, AttractionComment
+
 
 # Create your tests here.
+class AttractionTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser",
+            email="test@email.com",
+            password="secret",
+            country="Test Country",
+        )
+
+        cls.attraction = Attraction.objects.create(
+            name="TestLocation",
+            description="This is a test location",
+            address="Somewhere, Somplace",
+            author=cls.user,
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.attraction.name, "TestLocation")
+        self.assertEqual(self.attraction.description, "This is a test location")
+        self.assertEqual(self.attraction.address, "Tester Country")
+        self.assertEqual(self.attraction.image, "staticfiles/img/dest/no_image.jpg")
+        self.assertEqual(self.attraction.author.username, "testuser")
+
+    def test_url_exists_at_correct_location_listview(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_attraction_listview(self):
+        response = self.client.get(reverse("attraction_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "This is a test location")
+        self.assertTemplateUsed(response, "attraction_list.html")
+
+    def test_attraction_detailview(self):
+        response = self.client.get(
+            reverse("attraction_detail", kwargs={"pk": self.attraction.pk})
+        )
+        no_response = self.client.get("/attractions/{{self.attraction.pk}}/")
+        self.assertEqual(no_response.status_code, 404)
+
+    def test_attraction_createview(self):
+        response = self.client.post(
+            reverse("attraction_new"),
+            {
+                "name": "NewLocation",
+                "description": "This is a new location",
+                "address": "New Country",
+                "author": self.user.id,
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Attraction.objects.last("name"), "NewLocation")
+        self.assertEqual(
+            Attraction.objects.last("description"), "This is a new location"
+        )
+        self.assertEqual(Attraction.objects.last("address"), "New Country")
+        self.assertEqual(Attraction.objects.last("username"), "testuser")
+
+    def test_attraction_updateview(self):
+        response = self.client.post(
+            reverse("attraction_edit", args="1"),
+            {
+                "name": "UpdatedLocation",
+                "description": "Updated details about new location",
+                "address": "Updated country",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Attraction.objects.last("name"), "UpdatedLocation")
+        self.assertEqual(
+            Attraction.objects.last("description"), "Updated details about new location"
+        )
+        self.assertEqual(Attraction.objects.last("address"), "Updated Country")
+
+    def test_attraction_deleteview(self):
+        response = self.client.post(reverse("attraction_delete", args="1"))
+        self.assertEqual(response.status_code, 302)
+
+
+class AttractionCommentTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser",
+            email="test@email.com",
+            password="secret",
+            country="Test Country",
+        )
+
+        cls.attraction = Attraction.objects.create(
+            name="TestLocation",
+            description="This is a test location",
+            address="Tester Country",
+            author=cls.user,
+        )
+
+        cls.attractioncomment = AttractionComment.objects.create(
+            attraction=cls.attraction,
+            comment="A test comment",
+            author=cls.user,
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.attractioncomment.attraction.pk, "TestLocation")
+        self.assertEqual(self.attractioncomment.comment, "A test comment")
+        self.assertEqual(self.attraction.author.username, "testuser")
--- destinations/tests.py
+++ destinations/tests.py
@@ -9,8 +9,13 @@ from .models import Destination, DestinationComment
 class DestinationTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user = get_user_model().objects.create_user(username="testuser", email="test@email.com", password="secret", country="Test Country")
-        
+        cls.user = get_user_model().objects.create_user(
+            username="testuser",
+            email="test@email.com",
+            password="secret",
+            country="Test Country",
+        )
+
         cls.destination = Destination.objects.create(
             location="TestLocation",
             details="This is a test location",
@@ -38,7 +43,9 @@ class DestinationTests(TestCase):
         self.assertTemplateUsed(response, "destination_list.html")
 
     def test_destination_detailview(self):
-        response = self.client.get(reverse("destination_detail", kwargs={"pk": self.destination.pk}))
+        response = self.client.get(
+            reverse("destination_detail", kwargs={"pk": self.destination.pk})
+        )
         no_response = self.client.get("/destinations/{{self.destination.pk}}/")
         self.assertEqual(no_response.status_code, 404)
 
@@ -46,17 +53,17 @@ class DestinationTests(TestCase):
         response = self.client.post(
             reverse("destination_new"),
             {
-                "location":"NewLocation",
-                "details":"This is a new location",
-                "country":"New Country",
+                "location": "NewLocation",
+                "details": "This is a new location",
+                "country": "New Country",
                 "author": self.user.id,
             },
         )
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(Destination.objects.last().location, "NewLocation")
-        self.assertEqual(Destination.objects.last().details, "This is a new location")
-        self.assertEqual(Destination.objects.last().country, "New Country")
-        self.assertEqual(Destination.objects.last().username, "testuser")
+        self.assertEqual(Destination.objects.last("location"), "NewLocation")
+        self.assertEqual(Destination.objects.last("details"), "This is a new location")
+        self.assertEqual(Destination.objects.last("country"), "New Country")
+        self.assertEqual(Destination.objects.last("username"), "testuser")
 
     def test_destination_updateview(self):
         response = self.client.post(
@@ -68,20 +75,28 @@ class DestinationTests(TestCase):
             },
         )
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(Destination.objects.last().location, "UpdatedLocation")
-        self.assertEqual(Destination.objects.last().details, "Updated details about new location")
-        self.assertEqual(Destination.objects.last().country, "Updated Country")
+        self.assertEqual(Destination.objects.last("location"), "UpdatedLocation")
+        self.assertEqual(
+            Destination.objects.last("details"), "Updated details about new location"
+        )
+        self.assertEqual(Destination.objects.last("country"), "Updated Country")
 
     def test_destination_deleteview(self):
         response = self.client.post(reverse("destination_delete", args="1"))
         self.assertEqual(response.status_code, 302)
 
+
 class DestinationCommentTests(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.user = get_user_model().objects.create_user(username="testuser", email="test@email.com", password="secret", country="Test Country")
-        
-        cls.newdestination = Destination.objects.create(
+        cls.user = get_user_model().objects.create_user(
+            username="testuser",
+            email="test@email.com",
+            password="secret",
+            country="Test Country",
+        )
+
+        cls.destination = Destination.objects.create(
             location="TestLocation",
             details="This is a test location",
             country="Tester Country",
@@ -89,15 +104,12 @@ class DestinationCommentTests(TestCase):
         )
 
         cls.destinationcomment = DestinationComment.objects.create(
-            destination=cls.newdestination.pk,
+            destination=cls.destination,
             comment="A test comment",
             author=cls.user,
-        )            
+        )
 
     def test_model_content(self):
         self.assertEqual(self.destinationcomment.destination.pk, "TestLocation")
         self.assertEqual(self.destinationcomment.comment, "A test comment")
         self.assertEqual(self.destination.author.username, "testuser")
-        
-        
-                                   
